3845492f23c2976889938fb1dbcade3b7f9e918e
FriedrichWeinmann <friedrich.weinmann@powershell-stuttgart.de>
2017-06-30 08:12:54 +0200

    load published functions

--- PSFramework/PSFramework.psm1
+++ PSFramework/PSFramework.psm1
@@ -1,1 +1,8 @@
-﻿
+﻿$script:PSFrameworkModuleRoot = $PSScriptRoot
+
+
+# Import all public functions
+foreach ($function in (Get-ChildItem "$PSFrameworkModuleRoot\functions\*\*.ps1"))
+{
+	$ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
+}
