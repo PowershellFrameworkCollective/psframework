51508978da88c7b15c71e08acb4c28ab9895d143
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2017-08-02 15:58:41 +0200

    Added basic provider infrastructure

---
+++ library/PSFramework/Logging/Provider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSFramework.Logging
+{
+    /// <summary>
+    /// Class containing all information/content needed for a logging provider
+    /// </summary>
+    public class Provider
+    {
+        /// <summary>
+        /// Each logging provider has a name. Make sure it's unique
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// Whether the logging provider is enabled. Only when this is set to true will the logging script execute its events.
+        /// </summary>
+        public bool Enabled;
+
+        /// <summary>
+        /// Event that is executed once per logging script execution, before logging occurs
+        /// </summary>
+        public ScriptBlock BeginEvent;
+
+        /// <summary>
+        /// Event that is executed each logging cycle, right before processing items.
+        /// </summary>
+        public ScriptBlock StartEvent;
+
+        /// <summary>
+        /// Event that is executed for each message written
+        /// </summary>
+        public ScriptBlock MessageEvent;
+
+        /// <summary>
+        /// Event that is executed for each error written
+        /// </summary>
+        public ScriptBlock ErrorEvent;
+
+        /// <summary>
+        /// Event that is executed once per logging cycle, after processing all message and error items.
+        /// </summary>
+        public ScriptBlock EndEvent;
+
+        /// <summary>
+        /// Final Event that is executed when stopping the logging script.
+        /// </summary>
+        public ScriptBlock FinalEvent;
+
+        /// <summary>
+        /// Scriptblock that adds additional parameters as needed to the Set-PSFLoggingProvider function
+        /// </summary>
+        public ScriptBlock DynamicParam;
+
+        /// <summary>
+        /// The configuration steps taken by the Set-PSFLoggingProvider function
+        /// </summary>
+        public ScriptBlock ConfigurationEvent;
+    }
+}
---
+++ library/PSFramework/Logging/ProviderHost.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSFramework.Logging
+{
+    /// <summary>
+    /// Provides infrastructure services for the logging provider system
+    /// </summary>
+    public static class ProviderHost
+    {
+        /// <summary>
+        /// Dictionary with all registered logging providers
+        /// </summary>
+        public static Dictionary<string, Provider> Providers = new Dictionary<string, Provider>();
+
+        /// <summary>
+        /// Returns all enabled logging providers
+        /// </summary>
+        /// <returns>All enabled logging providers</returns>
+        public static List<Provider> GetEnabled()
+        {
+            List<Provider> list = new List<Provider>();
+            foreach (Provider prov in Providers.Values)
+                if (prov.Enabled)
+                    list.Add(prov);
+            return list;
+        }
+    }
+}
