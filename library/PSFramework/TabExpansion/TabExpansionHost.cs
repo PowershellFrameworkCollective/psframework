74afac7b8d595315eaa16c2a75161e24a8efbc53
FriedrichWeinmann <friedrich.weinmann@powershell-stuttgart.de>
2017-09-14 07:02:54 +0200

    Implementing basic Tab Expansion Support

---
+++ library/PSFramework/TabExpansion/TabExpansionHost.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace PSFramework.TabExpansion
+{
+    /// <summary>
+    /// Class that handles the static fields supporting the ÜSFramework TabExpansion implementation
+    /// </summary>
+    public static class TabExpansionHost
+    {
+        #region State information
+        /// <summary>
+        /// Field containing the scripts that were registered.
+        /// </summary>
+        public static ConcurrentDictionary<string, ScriptContainer> Scripts = new ConcurrentDictionary<string, ScriptContainer>();
+
+        /// <summary>
+        /// The cache used by scripts utilizing TabExpansionPlusPlus for PSFramework
+        /// </summary>
+        public static Hashtable Cache = new Hashtable();
+        #endregion State information
+    }
+}
