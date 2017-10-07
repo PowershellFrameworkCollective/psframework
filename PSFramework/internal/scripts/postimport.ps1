1ddfee037d204f8c3bdb469aa0fd4590b40069a2
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2017-10-07 21:00:29 +0200

    Updated path variable

--- PSFramework/internal/scripts/postimport.ps1
+++ PSFramework/internal/scripts/postimport.ps1
@@ -1,34 +1,34 @@
 ﻿# Initialize the configurations
-foreach ($file in (Get-ChildItem -Path "$PSFrameworkModuleRoot\internal\configurationvalidation\*.ps1"))
+foreach ($file in (Get-ChildItem -Path "$PSModuleRoot\internal\configurationvalidation\*.ps1"))
 {
 	. Import-ModuleFile -Path $file.FullName
 }
-foreach ($file in (Get-ChildItem -Path "$PSFrameworkModuleRoot\internal\configurations\*.ps1"))
+foreach ($file in (Get-ChildItem -Path "$PSModuleRoot\internal\configurations\*.ps1"))
 {
 	. Import-ModuleFile -Path $file.FullName
 }
 
 # Import configuration settings from registry
-. Import-ModuleFile -Path "$PSFrameworkModuleRoot\internal\scripts\loadConfigurationFromRegistry.ps1"
+. Import-ModuleFile -Path "$PSModuleRoot\internal\scripts\loadConfigurationFromRegistry.ps1"
 
 # Load each logging provider
-foreach ($file in (Get-ChildItem -Path "$PSFrameworkModuleRoot\internal\loggingProviders\"))
+foreach ($file in (Get-ChildItem -Path "$PSModuleRoot\internal\loggingProviders\"))
 {
 	. Import-ModuleFile -Path $file.FullName
 }
 
 # Start the logging system
-. Import-ModuleFile -Path "$PSFrameworkModuleRoot\internal\scripts\async-logging2.ps1"
+. Import-ModuleFile -Path "$PSModuleRoot\internal\scripts\async-logging2.ps1"
 
 # Launch the Tab Expansion system
-foreach ($file in (Get-ChildItem -Path "$PSFrameworkModuleRoot\internal\tepp\\scripts"))
+foreach ($file in (Get-ChildItem -Path "$PSModuleRoot\internal\tepp\\scripts"))
 {
 	. Import-ModuleFile -Path $file.FullName
 }
-. Import-ModuleFile -Path "$PSFrameworkModuleRoot\internal\tepp\tepp-assignment.ps1"
+. Import-ModuleFile -Path "$PSModuleRoot\internal\tepp\tepp-assignment.ps1"
 
 # Register the task engine
-. Import-ModuleFile -Path "$PSFrameworkModuleRoot\internal\scripts\taskEngine.ps1"
+. Import-ModuleFile -Path "$PSModuleRoot\internal\scripts\taskEngine.ps1"
 
 # Finally register the license
-. Import-ModuleFile -Path "$PSFrameworkModuleRoot\internal\scripts\license.ps1"
+. Import-ModuleFile -Path "$PSModuleRoot\internal\scripts\license.ps1"
