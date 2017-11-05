9e1415ff570811b15e6a2a243f47dbc3de18fad5
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2017-11-05 16:20:42 +0100

    fixed weblink

--- build/vsts-build.ps1
+++ build/vsts-build.ps1
@@ -24,9 +24,9 @@ Get-ChildItem "env:" | Out-Host
 
 Write-Host "########################################################################################################" -ForegroundColor DarkGreen
 
-Write-Host "Downloading from 'https://github.com/PowershellFrameworkCollective/psframework/blob/$($env:BUILD_SOURCEBRANCHNAME)/PSFramework/bin/PSFramework.dll'"
+Write-Host "Downloading from 'https://github.com/PowershellFrameworkCollective/psframework/raw/$($env:BUILD_SOURCEBRANCHNAME)/PSFramework/bin/PSFramework.dll'"
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile("https://github.com/PowershellFrameworkCollective/psframework/blob/$($env:BUILD_SOURCEBRANCHNAME)/PSFramework/bin/PSFramework.dll","$((Get-Location).Path)\PSFramework.dll")
+$wc.DownloadFile("https://github.com/PowershellFrameworkCollective/psframework/raw/$($env:BUILD_SOURCEBRANCHNAME)/PSFramework/bin/PSFramework.dll","$((Get-Location).Path)\PSFramework.dll")
 #Invoke-WebRequest -OutFile PSFramework.dll -Uri "https://github.com/PowershellFrameworkCollective/psframework/blob/$($env:BUILD_SOURCEBRANCHNAME)/PSFramework/bin/PSFramework.dll"
 
 Write-Host "Downloaded file has $((Get-Item PSFramework.dll).Length) bytes"
