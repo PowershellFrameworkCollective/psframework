9289a687e74f3ae945f56926d4a75284e19d7d54
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-07 20:09:56 +0100

    Updated logging provider configuration

--- PSFramework/functions/logging/Install-PSFLoggingProvider.ps1
+++ PSFramework/functions/logging/Install-PSFLoggingProvider.ps1
@@ -56,15 +56,20 @@
 		
 		if (-not ([PSFramework.Logging.ProviderHost]::Providers.ContainsKey($Name.ToLower())))
 		{
-			Stop-PSFFunction -Message "Provider $Name not found!" -EnableException $EnableException -Category InvalidArgument -Target $Name
+			Stop-PSFFunction -Message "Provider $Name not found!" -EnableException $EnableException -Category InvalidArgument -Target $Name -Tag 'logging', 'provider', 'install'
 			return
 		}
 		
 		$provider = [PSFramework.Logging.ProviderHost]::Providers[$Name.ToLower()]
 		
-		if (-not $provider.IsInstalledScript.Invoke())
+		if (-not ([System.Management.Automation.ScriptBlock]::Create($provider.IsInstalledScript).Invoke()))
 		{
-			[System.Management.Automation.ScriptBlock]::Create($provider.InstallationScript).Invoke()
+			try { [System.Management.Automation.ScriptBlock]::Create($provider.InstallationScript).Invoke() }
+			catch
+			{
+				Stop-PSFFunction -Message "Failed to install provider '$Name'" -EnableException $EnableException -Target $Name -ErrorRecord $_ -Tag 'logging', 'provider', 'install'
+				return
+			}
 		}
 	}
 	end
