0a09d6e0e44f60624b3574f9d05314e2fb897f7a
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-07 20:55:49 +0100

    Implemented unattended mode in library

--- PSFramework/internal/configurations/general.ps1
+++ PSFramework/internal/configurations/general.ps1
@@ -1,2 +1,2 @@
 ﻿# Unattended mode, so there is a central flag scripts & moduls can detect
-Set-PSFConfig -Module PSFramework -Name 'System.Unattended' -Value $false -Initialize -Validation "bool" -Handler {  } -Description "Central setting, showing whether the current execution is unattended or not. This allows scripts/moduls to react to whether there is a user at the controls or not."
+Set-PSFConfig -Module PSFramework -Name 'System.Unattended' -Value $false -Initialize -Validation "bool" -Handler { [PSFramework.PSFCore.PSFCoreHost]::Unattended = $args[0] } -Description "Central setting, showing whether the current execution is unattended or not. This allows scripts/moduls to react to whether there is a user at the controls or not."
