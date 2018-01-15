b29b977bc64cef1d818bef90deceb666168e3a79
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-15 22:54:37 +0100

    Updated build related stuff

---
+++ build/vsts-validate.ps1
@@ -0,0 +1,4 @@
+# Guide for available variables and working with secrets:
+# https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=powershell
+
+# Needs to ensure things are Done Right and only legal commits to master get built
