47884eec128c0e27344df004bd7b39386a0f3c1d
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-15 23:54:39 +0100

    fixed variable

--- build/vsts-build.ps1
+++ build/vsts-build.ps1
@@ -3,5 +3,5 @@
 	$WhatIf
 )
 
-if ($WhatIf) { Publish-Module "$(System.DefaultWorkingDirectory)\PSFramework" -NuGetApiKey $ApiKey -Force -WhatIf }
+if ($WhatIf) { Publish-Module "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)\PSFramework" -NuGetApiKey $ApiKey -Force -WhatIf }
 else { Publish-Module "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)\PSFramework" -NuGetApiKey $ApiKey -Force }
