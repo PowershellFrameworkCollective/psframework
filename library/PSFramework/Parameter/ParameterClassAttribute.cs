39cc2336925b9a8d69d9cd5495f97c77159d0e92
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-17 12:51:32 +0100

    Added parameter class code

---
+++ library/PSFramework/Parameter/ParameterClassAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PSFramework.Parameter
+{
+    /// <summary>
+    /// Attribute marking a class
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ParameterClassAttribute : Attribute
+    {
+    }
+}
