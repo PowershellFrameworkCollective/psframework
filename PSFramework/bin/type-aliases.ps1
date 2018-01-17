7dc982f1b43900cbd6763907da131b702ec7d580
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-17 13:09:47 +0100

    added extensions

---
+++ PSFramework/bin/type-aliases.ps1
@@ -0,0 +1,20 @@
+﻿# Obtain a reference to the TypeAccelerators type
+$TAType = [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")
+
+# Define our type aliases
+$TypeAliasTable = @{
+	PSFComputer  = "PSFramework.Parameter.ComputerParameter"
+	psfrgx       = "PSFramework.Utility.RegexHelper"
+}
+
+# Add all type aliases
+foreach ($TypeAlias in $TypeAliasTable.Keys)
+{
+	try
+	{
+		$TAType::Add($TypeAlias, $TypeAliasTable[$TypeAlias])
+	}
+	catch
+	{
+	}
+}
