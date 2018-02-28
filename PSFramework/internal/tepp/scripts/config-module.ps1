9396fbcebc7ec7098b268aedcb3f4e2a3a2f35d9
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-02-28 21:28:02 +0100

    Fixed tab Completion

--- PSFramework/internal/tepp/scripts/config-module.ps1
+++ PSFramework/internal/tepp/scripts/config-module.ps1
@@ -1,26 +1,3 @@
-﻿#region Tepp Data return: Module Name
-$ScriptBlock = {
-	param (
-		$commandName,
-		
-		$parameterName,
-		
-		$wordToComplete,
-		
-		$commandAst,
-		
-		$fakeBoundParameter
-	)
-	
-	$start = Get-Date
-	[PSFramework.TabExpansion.TabExpansionHost]::Scripts["config-module"].LastExecution = $start
-	
-	foreach ($name in ([PSFramework.Configuration.ConfigurationHost]::Configurations.Values.Module | Select-Object -Unique | Where-Object { $_ -Like "$wordToComplete*" } | Sort-Object))
-	{
-		New-PSFTeppCompletionResult -CompletionText $name -ToolTip $name
-	}
-	[PSFramework.TabExpansion.TabExpansionHost]::Scripts["config-module"].LastDuration = (Get-Date) - $start
+﻿Register-PSFTeppScriptblock -Name "PSFramework-config-module" -ScriptBlock {
+	[PSFramework.Configuration.ConfigurationHost]::Configurations.Values.Module | Select-Object -Unique
 }
-
-Register-PSFTeppScriptblock -ScriptBlock $ScriptBlock -Name "PSFramework-config-module"
-#endregion Tepp Data return: Module Name
