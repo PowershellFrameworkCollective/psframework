1bafc530915534c09516b6ecf34802cd2807c3e6
Friedrich Weinmann <Friedrich.Weinmann@powershell-stuttgart.de>
2018-06-20 22:21:52 +0200

    Fix policy/enforced flag

--- PSFramework/internal/scripts/loadConfigurationPersisted.ps1
+++ PSFramework/internal/scripts/loadConfigurationPersisted.ps1
@@ -9,8 +9,8 @@
 		{
 			if (-not $value.KeepPersisted) { Set-PSFConfig -FullName $value.FullName -Value $value.Value -EnableException }
 			else { Set-PSFConfig -FullName $value.FullName -PersistedValue $value.Value -PersistedType $value.Type -EnableException }
-			[PSFramework.Configuration.ConfigurationHost]::Configurations[$value.Name.ToLower()].PolicySet = $value.Policy
-			[PSFramework.Configuration.ConfigurationHost]::Configurations[$value.Name.ToLower()].PolicyEnforced = $value.Enforced
+			[PSFramework.Configuration.ConfigurationHost]::Configurations[$value.FullName.ToLower()].PolicySet = $value.Policy
+			[PSFramework.Configuration.ConfigurationHost]::Configurations[$value.FullName.ToLower()].PolicyEnforced = $value.Enforced
 		}
 		catch { }
 	}
