5e4d6a85b6222fffcc1c26bea0408c090a3eb78c
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-07-06 11:14:01 +0200

    Caught .NET trouble on Core for linux

--- PSFramework/functions/import/Register-PSFParameterClassMapping.ps1
+++ PSFramework/functions/import/Register-PSFParameterClassMapping.ps1
@@ -57,25 +57,33 @@
 	}
 	process
 	{
-		switch ($ParameterClass)
+		try
 		{
-			"Computer"
+			switch ($ParameterClass)
 			{
-				[PSFramework.Parameter.ComputerParameter]::SetTypePropertyMapping($TypeName.ToLower(), $Properties)
+				"Computer"
+				{
+					[PSFramework.Parameter.ComputerParameter]::SetTypePropertyMapping($TypeName.ToLower(), $Properties)
+				}
+				"DateTime"
+				{
+					[PSFramework.Parameter.DateTimeParameter]::SetTypePropertyMapping($TypeName.ToLower(), $Properties)
+				}
+				"TimeSpan"
+				{
+					[PSFramework.Parameter.TimeSpanParameter]::SetTypePropertyMapping($TypeName.ToLower(), $Properties)
+				}
+				default
+				{
+					Stop-PSFFunction -Message "Support for the $ParameterClass parameter class has not yet been added!" -EnableException $EnableException -Tag 'fail', 'argument' -Category NotImplemented
+					return
+				}
 			}
-			"DateTime"
-			{
-				[PSFramework.Parameter.DateTimeParameter]::SetTypePropertyMapping($TypeName.ToLower(), $Properties)
-			}
-			"TimeSpan"
-			{
-				[PSFramework.Parameter.TimeSpanParameter]::SetTypePropertyMapping($TypeName.ToLower(), $Properties)
-			}
-			default
-			{
-				Stop-PSFFunction -Message "Support for the $ParameterClass parameter class has not yet been added!" -EnableException $EnableException -Tag 'fail', 'argument' -Category NotImplemented
-				return
-			}
+		}
+		catch
+		{
+			Stop-PSFFunction -Message "Failed to update property mapping for $ParameterClass : $Typename. This is likely happening on some Linux distributions due to an underlying .NET issue and means the parameter class cannot be used." -EnableException $EnableException -Tag 'fail', '.NET' -ErrorRecord $_
+			return
 		}
 	}
 	end
