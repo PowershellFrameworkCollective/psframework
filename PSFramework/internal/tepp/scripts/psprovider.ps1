a74f24ab3656b2074e924bf7cf341a33bb869b8d
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-07-31 00:46:56 +0200

    encoding fix

--- PSFramework/internal/tepp/scripts/psprovider.ps1
+++ PSFramework/internal/tepp/scripts/psprovider.ps1
@@ -1,3 +1,3 @@
-Register-PSFTeppScriptblock -Name 'PSFramework-utility-psprovider' -ScriptBlock {
+﻿Register-PSFTeppScriptblock -Name 'PSFramework-utility-psprovider' -ScriptBlock {
 	(Get-PSProvider).Name
 }
