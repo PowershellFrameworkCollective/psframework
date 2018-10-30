982e65a12f11a837aa142492114a18a5eada75c4
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2018-10-30 13:43:52 +0100

    Added command: ConvertTo-PSFHashtable

---
+++ PSFramework/functions/utility/ConvertTo-PSFHashtable.ps1
@@ -0,0 +1,47 @@
+﻿function ConvertTo-PSFHashtable
+{
+<#
+	.SYNOPSIS
+		Converts an object into a hashtable.
+	
+	.DESCRIPTION
+		Converts an object into a hashtable.
+		Can exclude individual properties from being included.
+	
+	.PARAMETER Exclude
+		The propertynames to exclude.
+		Must be full property-names, no wildcard/regex matching.
+	
+	.PARAMETER InputObject
+		The object(s) to convert
+	
+	.EXAMPLE
+		PS C:\> Get-ChildItem | ConvertTo-PSFHashtable
+	
+		Scans all items in the current path and converts those objects into hashtables.
+#>
+	
+	[CmdletBinding()]
+	Param (
+		[string[]]
+		$Exclude,
+		
+		[Parameter(ValueFromPipeline = $true)]
+		$InputObject
+	)
+	
+	process
+	{
+		foreach ($item in $InputObject)
+		{
+			if ($null -eq $item) { continue }
+			$hashTable = @{ }
+			foreach ($property in $item.PSObject.Properties)
+			{
+				if ($property.Name -in $Exclude) { continue }
+				$hashTable[$property.Name] = $property.Value
+			}
+			$hashTable
+		}
+	}
+}
