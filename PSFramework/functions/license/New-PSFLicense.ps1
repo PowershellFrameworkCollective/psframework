34c1056dcd221f738e13a56920fc686102ed7bf3
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-12-01 10:02:03 +0100

    fixed trailing whitespace

--- PSFramework/functions/license/New-PSFLicense.ps1
+++ PSFramework/functions/license/New-PSFLicense.ps1
@@ -79,7 +79,7 @@
 		"@
 	
 		This registers the Awesome Test Product as licensed under the common FreeBSD license.
-#>	
+#>
 	[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low', HelpUri = 'https://psframework.org/documentation/commands/PSFramework/New-PSFLicense')]
 	[OutputType([PSFramework.License.License])]
 	param
