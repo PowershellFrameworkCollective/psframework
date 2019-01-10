9ca09d569976fe4400f55aa328cce11f767df6d3
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2019-01-10 14:21:02 +0100

    minor updates

---
+++ PSFramework/internal/tepp/scripts/tepp.ps1
@@ -0,0 +1,18 @@
+﻿Register-PSFTeppScriptblock -Name 'PSFramework-tepp-scriptblockname' -ScriptBlock {
+	[PSFramework.TabExpansion.TabExpansionHost]::Scripts.Keys
+}
+
+Register-PSFTeppScriptblock -Name 'PSFramework-tepp-parametername' -ScriptBlock {
+	if ($fakeBoundParameter.Command)
+	{
+		$common = 'Verbose', 'Debug', 'ErrorAction', 'WarningAction', 'InformationAction', 'ErrorVariable', 'WarningVariable', 'InformationVariable', 'OutVariable', 'OutBuffer', 'PipelineVariable', 'WhatIf', 'Confirm'
+		
+		try
+		{
+			$command = Get-Command $fakeBoundParameter.Command
+			if ($command -is [System.Management.Automation.AliasInfo]) { $command = $command.ResolvedCommand }
+			$command.Parameters.Keys | Where-Object { $_ -notin $common }
+		}
+		catch { }
+	}
+}
