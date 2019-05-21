6d9e8dd8e5f450de005f5fd5d6df3b517b260d68
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2019-05-21 08:06:17 +0200

    Update vsts-prerequisites.ps1

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -2,5 +2,7 @@ Write-Host "Installing Pester" -ForegroundColor Cyan
 Install-Module Pester -Force -SkipPublisherCheck
 Write-Host "Installing PSScriptAnalyzer" -ForegroundColor Cyan
 Install-Module PSScriptAnalyzer -Force -SkipPublisherCheck
+Write-Host "Installing latest PackageManagement" -ForegroundColor Cyan
+Install-Module PackageManagement -Force -SkipPublisherCheck
 Write-Host "Installing latest PowerShellGet" -ForegroundColor Cyan
 Install-Module PowerShellGet -Force -SkipPublisherCheck
