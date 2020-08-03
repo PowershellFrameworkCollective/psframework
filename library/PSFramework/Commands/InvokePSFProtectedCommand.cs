9827e97e8566084c9c3faa9833542aa7f2c26d94
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2020-08-03 09:48:04 +0200

    updates

--- library/PSFramework/Commands/InvokePSFProtectedCommand.cs
+++ library/PSFramework/Commands/InvokePSFProtectedCommand.cs
@@ -222,10 +222,10 @@ return
 
                 try
                 {
-                    object result = PSCmdlet.InvokeCommand.InvokeScript(false, ScriptBlock, null, null);
+                    var result = PSCmdlet.InvokeCommand.InvokeScript(false, ScriptBlock, null, null);
                     WriteMessage(Localization.LocalizationHost.Read("PSFramework.FlowControl.Invoke-PSFProtectedCommand.Success", new object[] { _Message }), Message.MessageLevel.SomewhatVerbose, _Caller.CallerFunction, _Caller.CallerModule, _Caller.CallerFile, _Caller.CallerLine, Tag, Target);
-                    if (result != null)
-                        WriteObject(result);
+                    if (result != null && result.Count > 0)
+                        WriteObject(result, true);
                     return;
                 }
                 catch (Exception e)
