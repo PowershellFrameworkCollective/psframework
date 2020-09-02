604c9bb16ceffddde06f061afe3111bec0dea4b9
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2020-09-02 21:37:50 +0200

    bugfix & build system update

--- PSFramework/internal/scripts/preimport.ps1
+++ PSFramework/internal/scripts/preimport.ps1
@@ -1,7 +1,7 @@
-﻿# Load "Environment" variables within the module
-[PSFramework.PSFCore.PSFCoreHost]::WriteDebug("  Environment", "")
-. Import-ModuleFile -Path "$($script:ModuleRoot)\internal\scripts\environment.ps1"
+﻿$moduleRoot = Split-Path (Split-Path $PSScriptRoot)
+
+# Load "Environment" variables within the module
+"$($moduleRoot)\internal\scripts\environment.ps1"
 
 # Load Tab Expansion Plus Plus code (PS4 or older)
-[PSFramework.PSFCore.PSFCoreHost]::WriteDebug("  TEPP Core", "")
-. Import-ModuleFile -Path "$($script:ModuleRoot)\internal\scripts\teppCoreCode.ps1"
+"$($moduleRoot)\internal\scripts\teppCoreCode.ps1"
