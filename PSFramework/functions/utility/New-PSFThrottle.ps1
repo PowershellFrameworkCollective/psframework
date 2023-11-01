c4e8470e0f1b1f5b56244d54f9d3bac29a12b140
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2023-11-01 11:18:21 +0100

    multiple throttle limits

--- PSFramework/functions/utility/New-PSFThrottle.ps1
+++ PSFramework/functions/utility/New-PSFThrottle.ps1
@@ -15,14 +15,25 @@
 	
 	.PARAMETER Interval
 		The time range during which we measure slot/execution limits.
+		Can be either a timespan or a human-friendly notation such as "10m" for 10 minutes or "1h" for one hour.
 	
 	.PARAMETER Limit
 		The number of valid executions within the specified interval.
+
+	.PARAMETER ExtraLimits
+		Additional Interval/Limit pairs to throttle over at the same time.
+		This allows logic such as "Not more than 10 per minute and 100 per hour".
+		The keys of the hashtable are the interval (same type as the Interval parameter), the values the limit (int)
 	
 	.EXAMPLE
 		PS C:\> New-PSFThrottle -Interval 1m -Limit 60
 	
 		Generates a throttle object that will allow 60 slots/executions every minute.
+
+	.EXAMPLE
+		PS C:\> New-PSFThrottle -Interval 1m -Limit 60 -ExtraLimits @{ '1h' = 600 }
+
+		Generates a throttle object that will allow 60 slots/executions every minute and 600 every hour.
 #>
 	[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
 	[CmdletBinding()]
@@ -33,10 +44,17 @@
 		
 		[Parameter(Mandatory = $true)]
 		[int]
-		$Limit
+		$Limit,
+
+		[hashtable]
+		$ExtraLimits = @{}
 	)
 	
 	process {
-		New-Object PSFramework.Utility.Throttle($Limit, $Interval)
+		$throttle = New-Object PSFramework.Utility.Throttle($Limit, $Interval)
+		foreach ($pair in $ExtraLimits.GetEnumerator()) {
+			try { $throttle.AddLimit($pair.Key, $pair.Value) }
+			catch { Write-PSFMessage -Level Error -String 'New-PSFThrottle.Error.InvalidLimit' -StringValues $pair.Key, $pair.Value -ErrorRecord $_ -EnableException $true -PSCmdlet $PSCmdlet }
+		}
 	}
 }
