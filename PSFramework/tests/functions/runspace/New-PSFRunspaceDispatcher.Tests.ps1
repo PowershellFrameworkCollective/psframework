5e64809684cb2372421929c37d7d849e0ece9cec
FriedrichWeinmann <FriedrichWeinmann@users.noreply.github.com>
2023-11-06 00:06:07 +0100

    Update New-PSFRunspaceDispatcher.Tests.ps1

--- PSFramework/tests/functions/runspace/New-PSFRunspaceDispatcher.Tests.ps1
+++ PSFramework/tests/functions/runspace/New-PSFRunspaceDispatcher.Tests.ps1
@@ -6,24 +6,24 @@
 		& (Get-Module PSFramework) { $script:runspaceWorkflows = @{ } }
 	}
 
-	It "Should create a new Runspace Dispatcher without error" {
+	It "Should create a new Runspace Workflow without error" {
 		{ New-PSFRunspaceWorkflow -Name "Test" } | Should -Not -Throw
 	}
-	It "Should return a dispatcher object with the correct name and no workers" {
+	It "Should return a workflow object with the correct name and no workers" {
 		$workflow = New-PSFRunspaceWorkflow -Name "Test"
 		$workflow | Should -Not -BeNullOrEmpty
 		$workflow.Name | Should -Be 'Test'
 		$workflow.Workers.Count | Should -Be 0
 	}
-	It "Should refuse creating a dispatcher that already exists" {
+	It "Should refuse creating a workflow that already exists" {
 		$null = New-PSFRunspaceWorkflow -Name "Test"
 		{ New-PSFRunspaceWorkflow -Name "Test" } | Should -Throw
 	}
-	It "Should overwrite a dispatcher that already exists when using Force" {
+	It "Should overwrite a workflow that already exists when using Force" {
 		$workflow = New-PSFRunspaceWorkflow -Name "Test"
 		$workflow.Queues.Test.Enqueue(42)
 		{ New-PSFRunspaceWorkflow -Name "Test" -Force } | Should -Not -Throw
-		$newDispatcher = Get-PSFRunspaceWorkflow -Name Test
-		$newDispatcher.Queues.Test.Count | Should -Be 0
+		$newWorkflow = Get-PSFRunspaceWorkflow -Name Test
+		$newWorkflow.Queues.Test.Count | Should -Be 0
 	}
 }
